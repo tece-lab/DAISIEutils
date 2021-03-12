9c5928e68b605edd770e2b337fb3729ea70fc5b4
Neves-P <p.m.santos.neves@rug.nl>
2021-03-12 02:56:51 +0100

    Add default_params_doc.R Also document print_main_header().

---
+++ R/default_params_doc.R
@@ -0,0 +1,14 @@
+#' Default parameters documentation
+#'
+#' @param run_name Character with name of run to be printed. Can be anything,
+#'   altough it is useful to name it after the dataset that is being analysed or
+#'   generated.
+#' @param M Positive numeric integer with mainland species number.
+#' @param metadata A numeric or a string. To be improved to take more info.
+#' @return Nothing
+#' @export
+default_params_doc <- function(run_name,
+                               M,
+                               metadata) {
+  # Nothing
+}

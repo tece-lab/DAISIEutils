bb4c5fc8bb0a37cd3f1388e93f25129c95f86ee8
Neves-P <p.m.santos.neves@rug.nl>
2021-03-14 23:51:47 +0100

    Test verbose

--- tests/testthat/test-create_output_folder.R
+++ tests/testthat/test-create_output_folder.R
@@ -6,3 +6,21 @@ test_that("Directory is created", {
   # Clean-up
   expect_equal(unlink(file.path(getwd(), "results"), recursive = TRUE), 0)
 })
+
+test_that("Verbose sends message", {
+
+  output_name <- "test"
+  expect_message(
+    file_name <- create_output_folder(
+      output_name = output_name,
+      model = 1,
+      verbose = TRUE
+    ),
+    paste0(file.path(getwd(), "results", output_name),
+           " not found, creating it.")
+  )
+  # Clean-up
+  expect_equal(unlink(file.path(getwd(), "results"), recursive = TRUE), 0)
+})
+
+

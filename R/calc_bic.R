c2fb5d24ed7327e3c6d774720e4bea6ef5be3350
Neves-P <p.m.santos.neves@rug.nl>
2021-03-26 16:22:09 +0100

    Fix calc_bic

--- R/calc_bic.R
+++ R/calc_bic.R
@@ -6,8 +6,9 @@
 #' @export
 #'
 #' @author Joshua W. Lambert, Pedro Neves
-calc_bic <- function(results, m) {
+calc_bic <- function(results, data) {
   k <- results$df
+  m <- data$not_present + length(data) - 1
   bic <- k * (log(m) + log(2 * pi)) - 2 * results$loglik
   bic
 }

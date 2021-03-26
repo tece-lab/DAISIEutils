8b936a0de486ce90afcdbf8da61597037f3bb8a4
Neves-P <p.m.santos.neves@rug.nl>
2021-03-26 16:22:20 +0100

    Use cond and numeric

--- scripts/run_analysis.R
+++ scripts/run_analysis.R
@@ -4,4 +4,5 @@ data(args[1], package = args[3])
 
 DAISIEutils::run_analysis(data = args[1],
                           model = args[2],
-                          seed = args[4])
+                          seed = as.numeric(args[4]),
+                          cond = as.numeric(args[5]))

fe1496a244449ab7ab650e0cc039e0ddb9f8a354
Neves-P <p.m.santos.neves@rug.nl>
2021-03-27 01:53:55 +0100

    Unskipped tests pass locally

--- tests/testthat/test-create_output_folder.R
+++ tests/testthat/test-create_output_folder.R
@@ -1,24 +1,18 @@
 test_that("Directory is created", {
-
-  file_name <- create_output_folder(output_name = "test", model = 1)
-  expect_equal(file_name, file.path(getwd(), "results", "test", "test_m_1.txt"))
+  data_name <- "test"
+  model <- "cr_dd"
+  seed <- 1
+  file_name <- create_output_folder(
+    data_name = data_name,
+    model = model,
+    seed = seed
+  )
+  expect_equal(
+    file_name,
+    file.path(getwd(), "results", "test", "test_cr_dd_1.rds")
+  )
   expect_true(dir.exists(file.path(getwd(), "results", "test")))
   # Clean-up
   expect_equal(unlink(file.path(getwd(), "results"), recursive = TRUE), 0)
 })
 
-test_that("Verbose sends message", {
-
-  output_name <- "test"
-  expect_message(
-    file_name <- create_output_folder(
-      output_name = output_name,
-      model = 1,
-      verbose = TRUE
-    ),
-    paste0(file.path(getwd(), "results", output_name),
-           " not found, creating it.")
-  )
-  # Clean-up
-  expect_equal(unlink(file.path(getwd(), "results"), recursive = TRUE), 0)
-})

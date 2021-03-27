1b0bfcf033ed948feac4bf66c2b8cb01abe0ebef
Neves-P <p.m.santos.neves@rug.nl>
2021-03-27 03:45:58 +0100

    $not_present is in data[[1]]

--- R/calc_bic.R
+++ R/calc_bic.R
@@ -8,7 +8,7 @@
 #' @author Joshua W. Lambert, Pedro Neves
 calc_bic <- function(results, data) {
   k <- results$df
-  m <- data$not_present + length(data) - 1
+  m <- data[[1]]$not_present + length(data) - 1
   bic <- k * (log(m) + log(2 * pi)) - 2 * results$loglik
   bic
 }

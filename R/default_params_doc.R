528e2aea6624c15fe996aacd8227d6c266888049
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-29 20:38:31 +0200

    updated documentation

--- R/default_params_doc.R
+++ R/default_params_doc.R
@@ -9,6 +9,36 @@
 #'     anagenesis (lama fixed to zero). All other parameters free.
 #'   * `"cr_di_0lama"` Clade specific model - diversity independent (K = Inf)
 #'     and no anagenesis (lama fixed to zero). All other parameters free.
+#'   * `"cr_di_0lamc"` Clade specific model - diversity indendent (K = Inf)
+#'     and no cladogenesis (lamc fixed to zero). All other parameters free.
+#'   * `"cr_dd_0lamc"` Clade specific model - diversity dependent and no
+#'     cladogenesis (lamc fixed to zero). All other parameters free.
+#'   * `"rr_lamc_di"` Clade specific model - diversity independent (K = Inf)
+#'     and relaxed cladogenesis. All other parameters free.
+#'   * `"rr_lamc_dd"` Clade specific model - diversity dependent and relaxed
+#'     cladogenesis. All other parameters free.
+#'   * `"rr_mu_di"` Clade specific model - diversity independent (K = Inf) and
+#'     relaxed extinction. All other parameters free.
+#'   * `"rr_mu_dd"` Clade specific model - diversity dependent and relaxed
+#'     extinction. All other parameters free.
+#'   * `"rr_k"` Clade specific model - diversity dependent and relaxed carrying
+#'     capacity. All other parameters free.
+#'   * `"rr_lama_di"` Clade specific model - diversity independent (K = Inf)
+#'     and relaxed anagenesis. All other parameters free.
+#'   * `"rr_lama_dd"` Clade specific model - diversity dependent and relaxed
+#'     anagenesis. All other parameters free.
+#'   * `"rr_mu_di_0lamc"` Clade specific model - diversity independent,
+#'     relaxed extinction, and no cladogenesis (lamc fixed to zero). All other
+#'     parameters free.
+#'   * `"rr_mu_dd_0lamc"` Clade specific model - diversity dependent,
+#'   * `"rr_k_0lamc"` Clade specific model - diversity dependent,
+#'   * `"rr_lama_di_0lamc"` Clade specific model - diversity independent
+#'   * `"r_lama_dd_0lamc"` Clade specific model - diversity dependent
+#'   * `"rr_lamc_di_0lama"` Clade specific model - diversity independent
+#'   * `"rr_lamc_dd_0lama"` Clade specific model - diversity dependent
+#'   * `"rr_mu_di_0lama"` Clade specific model - diversity independent
+#'   * `"rr_mu_dd_0lama"` Clade specific model - diversity dependent
+#'   * `"rr_k_0lama"` Clade specific model - diversity dependent
 #' @param data_name String. Will be used for the name of the created output
 #'   folder.
 #' @param results_root_folder Character. A path to the root folder containing

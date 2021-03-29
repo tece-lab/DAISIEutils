b9b98f498d9948e7927f54b4a9683790a297343b
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-29 21:36:27 +0200

    changed parametes to greek

--- tests/testthat/test-setup_model.R
+++ tests/testthat/test-setup_model.R
@@ -6,19 +6,18 @@ test_that("setup_model is correct for cr_dd", {
   )
   expected_arguments <- list(
     ddmodel = 11,
-    idparsopt = c(clado = 1, ex = 2, k = 3, immig = 4, ana = 5),
+    idparsopt = c(lac = 1, mu = 2, k = 3, gam = 4, laa = 5),
     parsfix = NULL,
     idparsfix = NULL,
     idparsnoshift = 6:10,
     initparsopt = c(
-      clado = 5.31017326284e-01,
-      ex = 7.44247799274e-01,
+      lac = 5.31017326284e-01,
+      mu = 7.44247799274e-01,
       k = 1.17133552490e+02,
-      immig = 9.08207789995e-05,
-      ana = 8.86559531046e-01 ),
+      gam = 9.08207789995e-05,
+      laa = 8.86559531046e-01 ),
     cs_version = 1
   )
-
   expect_equal(model_arguments, expected_arguments)
 })
 
@@ -30,56 +29,55 @@ test_that("setup_model is correct for cr_di", {
   )
   expected_arguments <- list(
     ddmodel = 0,
-    idparsopt = c(clado = 1, ex = 2, immig = 4, ana = 5),
+    idparsopt = c(lac = 1, mu = 2, gam = 4, laa = 5),
     parsfix = Inf,
     idparsfix = 3,
     idparsnoshift = 6:10,
-    initparsopt = c(clado = 5.31017326284e-01,
-                    ex = 7.44247799274e-01,
-                    immig = 9.08207789995e-05,
-                    ana = 8.86559531046e-01),
+    initparsopt = c(lac = 5.31017326284e-01,
+                    mu = 7.44247799274e-01,
+                    gam = 9.08207789995e-05,
+                    laa = 8.86559531046e-01),
     cs_version = 1
   )
-
   expect_equal(model_arguments, expected_arguments)
 })
 
-test_that("setup_model is correct for cr_dd_0lama", {
+test_that("setup_model is correct for cr_dd_0laa", {
   set.seed(1)
-  model <- "cr_dd_0lama"
+  model <- "cr_dd_0laa"
   model_arguments <- setup_model(
     model = model
   )
   expected_arguments <- list(
     ddmodel = 11,
-    idparsopt = c(clado = 1, ex = 2, k = 3, immig = 4),
+    idparsopt = c(lac = 1, mu = 2, k = 3, gam = 4),
     parsfix = 0,
     idparsfix = 5,
     idparsnoshift = 6:10,
-    initparsopt = c(clado = 5.31017326284e-01,
-                    ex = 7.44247799274e-01,
+    initparsopt = c(lac = 5.31017326284e-01,
+                    mu = 7.44247799274e-01,
                     k = 1.17133552490e+02,
-                    immig = 9.08207789995e-05),
+                    gam = 9.08207789995e-05),
     cs_version = 1
   )
   expect_equal(model_arguments, expected_arguments)
 })
 
-test_that("setup_model is correct for cr_dd_0lama", {
+test_that("setup_model is correct for cr_dd_0laa", {
   set.seed(1)
-  model <- "cr_di_0lama"
+  model <- "cr_di_0laa"
   model_arguments <- setup_model(
     model = model
   )
   expected_arguments <- list(
     ddmodel = 0,
-    idparsopt = c(clado = 1, ex = 2, immig = 4),
+    idparsopt = c(lac = 1, mu = 2, gam = 4),
     parsfix = c(Inf, 0),
     idparsfix = c(3, 5),
     idparsnoshift = 6:10,
-    initparsopt = c(clado = 5.31017326284e-01,
-                    ex = 7.44247799274e-01,
-                    immig = 9.08207789995e-05),
+    initparsopt = c(lac = 5.31017326284e-01,
+                    mu = 7.44247799274e-01,
+                    gam = 9.08207789995e-05),
     cs_version = 1
   )
   expect_equal(model_arguments, expected_arguments)

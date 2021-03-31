cb7e2c91dab68a5a157e1cabba1ffa5d602230cc
Neves-P <p.m.santos.neves@rug.nl>
2021-04-01 01:06:42 +0200

    Load and cleanly delete test file

--- tests/testthat/test-calc_power.R
+++ tests/testthat/test-calc_power.R
@@ -1,9 +1,27 @@
 test_that("calc_power produces correct output", {
   skip("WIP")
   data("Galapagos_datalist", package = "DAISIE")
+
+  model <- "cr_dd"
+  seed <- 1
+  cond <- 1
+  results_folder <- dirname(create_output_folder(
+    data_name = data_name,
+    model = model,
+    seed = seed
+  ))
+
+  # Place files need to run bootstrap
+  reference_files <- list.files("tests/testdata/", full.names = TRUE)
+  expect_true(all(file.copy(reference_files, results_folder)))
+
   output <- calc_power(data = Galapagos_datalist)
   expected_output <- 0.904095904096
   expect_equal(output, expected_output)
+
+  # Delete temp folder
+  expect_equal(unlink(file.path(getwd(), "results"), recursive = TRUE), 0)
+
 })
 
 test_that("calc_power produces error", {

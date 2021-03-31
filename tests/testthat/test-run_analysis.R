1145d06b90b57d516ce7c3e742ab385bc584d337
Neves-P <p.m.santos.neves@rug.nl>
2021-03-31 19:44:56 +0200

    Fix reference values

--- tests/testthat/test-run_analysis.R
+++ tests/testthat/test-run_analysis.R
@@ -19,15 +19,15 @@ test_that("integration test", {
     getwd(), "results", "Galapagos_datalist", "Galapagos_datalist_cr_dd_1.rds")
   )
   expected_data_frame <- data.frame(
-    lambda_c = 2.57838865507069,
-    mu = 2.70482929613855,
-    K = 3074.82843572708,
-    gamma = 0.00935776284898708,
-    lambda_a = 1.00321372406581,
-    loglik = -75.9999607726643,
+    lambda_c = 2.556237280343648,
+    mu = 2.686757075111843,
+    K = 9245.783416787153,
+    gamma = 0.009325448643906421,
+    lambda_a = 1.007726332102268,
+    loglik = -75.99980195099431,
     df = 5,
     conv = 0,
-    bic = 195.728083272286)
+    bic = 195.727765628946)
   expect_equal(obtained_result, expected_data_frame)
   # Clean-up
   expect_equal(unlink("results", recursive = TRUE), 0)

876e5f4d08337e839425d130bd44096c8997c4a2
Neves-P <p.m.santos.neves@rug.nl>
2021-04-01 01:06:33 +0200

    snapshot bootstrap test WIP

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -1,15 +1,28 @@
 test_that("bootstrap works", {
   data_name <- "Galapagos_datalist"
+  data(Galapagos_datalist, package = "DAISIE")
   model <- "cr_dd"
   seed <- 1
+  cond <- 1
   results_folder <- dirname(create_output_folder(
     data_name = data_name,
     model = model,
     seed = seed
   ))
 
+  # Place files need to run bootstrap
   reference_files <- list.files("tests/testdata/", full.names = TRUE)
   expect_true(all(file.copy(reference_files, results_folder)))
 
+
+  expect_snapshot_file(bootstap(
+    data = Galapagos_datalist,
+    model_1 = "cr_dd",
+    model_2 = "cr_di",
+    seed = seed,
+    cond = cond),
+    "tests/testdata/Galapagos_datalist_boot_1.rds", binary = TRUE)
+
+  # Delete temp folder
   expect_equal(unlink(file.path(getwd(), "results"), recursive = TRUE), 0)
 })

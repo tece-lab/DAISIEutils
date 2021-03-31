347f609718493d7b7e6c184d3bf4c0a0f75b65c7
Neves-P <p.m.santos.neves@rug.nl>
2021-03-31 17:29:54 +0200

    Add choose_best_model

---
+++ R/choose_best_model.R
@@ -0,0 +1,16 @@
+#' From multiple seeds, choose the best model fit
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return A list of length one, with a data frame of 1 row containing the best
+#'   estimated models from the given seeds, as determined by the loglik.
+#' @export
+#'
+#' @author Pedro Neves
+choose_best_model <- function(model_lik_res) {
+  logliks <- unlist(lapply(X = model_lik_res, FUN = "[[", "loglik"))
+  best_loglik_index <- which(logliks == max(logliks, na.rm = TRUE))
+  best_model <- model_lik_res[best_loglik_index]
+  testit::assert(length(best_model) == 1)
+  return(best_model)
+}

00032e83ba61bbcdd7f9ef3992db9966c3aba54d
joshwlambert <j.w.l.lambert@rug.nl>
2021-03-31 14:18:29 +0200

    updated calc_p_value and calc_power

--- R/calc_p_value.R
+++ R/calc_p_value.R
@@ -10,7 +10,7 @@
 calc_p_value <- function(
   data
 ) {
-
+browser()
   data_name <- deparse(substitute(data))
   if (is_on_cluster()) {
     output_folder <- file.path(
@@ -19,19 +19,17 @@ calc_p_value <- function(
   } else {
     output_folder <- file.path(getwd(), "results", data_name)
   }
-  # load all from a data set
-  # make a list one RDS per element
-  apply(readRDS, ) #TODO finish
 
-  # extract all the lik_ratio_0
-  testit::assert(all(lik_ratio_0) == lik_ratio_0[1])
+  files <- list.files(path = output_folder, full.names = TRUE)
+  boot_files <- files[which(grepl("boot", files))]
+  list_res <- lapply(files, readRDS)
+
+  lik_ratio_0 <- unlist(lapply(list_res, '[[', 3))
   testit::assert(is.numeric(lik_ratio_0))
+  testit::assert(var(lik_ratio_0, na.rm = TRUE) < 1e-5)
 
-  lik_ratio_0 <- "stub"
-  # extract all the lik_ratio_1
-  testit::assert(is.numeric(lik_ratio_model_1))
-
-  lik_ratio_1 <- "stub"
+  lik_ratio_1 <- unlist(lapply(list_res, '[[', 7))
+  testit::assert(is.numeric(lik_ratio_1))
 
   r_model_1 <- length(which(lik_ratio_1 > lik_ratio_0))
   p_value <- (r_model_1 + 1) / (length(lik_ratio_1) + 1)
--- R/calc_power.R
+++ R/calc_power.R
@@ -10,14 +10,35 @@ calc_power <- function(
   data
 ) {
 
+
+  data_name <- deparse(substitute(data))
+  if (is_on_cluster()) {
+    output_folder <- file.path(
+      Sys.getenv("HOME"), "results", data_name
+    )
+  } else {
+    output_folder <- file.path(getwd(), "results", data_name)
+  }
+
+
+  files <- list.files(path = output_folder, full.names = TRUE)
+  boot_files <- files[which(grepl("boot", files))]
+  list_res <- lapply(files, readRDS)
+
+
   # extract lik_ratio_1
+  lik_ratio_1 <- unlist(lapply(list_res, '[[', 7))
+  testit::assert(is.numeric(lik_ratio_1))
+
 
   # calculate lik_ratio_alpha
+  testit::assert(is.numeric(lik_ratio_alpha))
 
   # extract lik_ratio_2
 
-  testit::assert(is.numeric(lik_ratio_alpha))
-  testit::assert(is.numeric(lik_ratio_model_2))
+  lik_ratio_2 <- unlist(lapply(list_res, '[[', 10))
+  testit::assert(is.numeric(lik_ratio_2))
+
   r_model_2 <- length(which(lik_ratio_model_2 > lik_ratio_alpha))
   power <- r_model_2 / (length(lik_ratio_model_2) + 1)
   return(power)

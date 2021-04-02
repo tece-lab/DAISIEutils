a5236099e1e9d94c8e33d6383441b2446885f26a
joshwlambert <j.w.l.lambert@rug.nl>
2021-04-02 16:57:26 +0200

    added sensitivity function

---
+++ R/sensitivity.R
@@ -0,0 +1,17 @@
+sensitivity <- function(data) {
+
+
+  # load data from models
+
+  # rank models from each data set
+
+  # compare ranks
+
+  #
+
+  # Take multiple data sets
+  # Take a set of models
+  # Fit the data sets to the models
+  # Determine whether the best fit model is consistent
+  # Determine whether the rank of the models fit is consistent
+}

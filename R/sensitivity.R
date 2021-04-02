9f6da5b02402dd0f123d6dd9008429fcb31f1a15
joshwlambert <j.w.l.lambert@rug.nl>
2021-04-02 17:41:03 +0200

    updated sensitivity

--- R/sensitivity.R
+++ R/sensitivity.R
@@ -1,8 +1,30 @@
-sensitivity <- function(data) {
+sensitivity <- function(
+  data_1,
+  data_2) {
+browser()
+  data_name <- deparse(substitute(data))
+  if (is_on_cluster()) {
+    output_folder <- file.path(
+      Sys.getenv("HOME"), "results", data_name
+    )
+  } else {
+    output_folder <- file.path(getwd(), "results", data_name)
+  }
 
+  data_1_files <- list.files(
+    path = output_folder,
+    full.names = TRUE,
+    pattern = paste0(model_1, "_[0-9].rds$"))
+  model_1_lik_res <- lapply(model_1_files, readRDS)
 
-  # load data from models
+  data_2_files <- list.files(
+    path = output_folder,
+    full.names = TRUE,
+    pattern = paste0(model_2, "_[0-9].rds$"))
+  model_2_lik_res <- lapply(model_2_files, readRDS)
 
+  best_model_1 <- choose_best_model(model_1_lik_res)
+  best_model_2 <- choose_best_model(model_2_lik_res)
   # rank models from each data set
 
   # compare ranks

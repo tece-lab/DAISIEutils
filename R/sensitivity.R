0f0747094e556d2a3c28731a18748657de125553
joshwlambert <j.w.l.lambert@rug.nl>
2021-04-05 17:59:11 +0200

    updated sensitivity and documentation

--- R/sensitivity.R
+++ R/sensitivity.R
@@ -1,39 +1,98 @@
+#' Read the results of [run_analysis()] and compares model selection to determine
+#' sensitivity to different data input into the same model or set of models.
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return List
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' sensitivity(
+#'   data_names = c("Azores", "Azores_alt_m"),
+#'   full_output = FALSE
+#' )
+#' }
 sensitivity <- function(
-  data_1,
-  data_2) {
-browser()
-  data_name <- deparse(substitute(data))
-  if (is_on_cluster()) {
-    output_folder <- file.path(
-      Sys.getenv("HOME"), "results", data_name
-    )
+  data_names,
+  full_output) {
+
+  best_models_list <- list()
+  output <- list()
+  for (i in seq_along(data_names)) {
+    if (is_on_cluster()) {
+      output_folder <- file.path(
+        Sys.getenv("HOME"), "results", data_names[i]
+      )
+    } else {
+      output_folder <- file.path(getwd(), "results", data_names[i])
+    }
+    expected_models <- c("cr_dd", "cr_di", "cr_dd_0laa", "cr_di_0laa",
+                         "cr_di_0lac", "cr_dd_0lac", "rr_lac_di",
+                         "rr_lac_dd", "rr_mu_di", "rr_mu_dd", "rr_k",
+                         "rr_laa_di", "rr_laa_dd", "rr_mu_di_0lac",
+                         "rr_mu_dd_0lac", "rr_k_0lac", "rr_laa_di_0lac",
+                         "rr_laa_dd_0lac", "rr_lac_di_0laa",
+                         "rr_lac_dd_0laa", "rr_mu_di_0laa", "rr_mu_dd_0laa",
+                         "rr_k_0laa")
+
+    best_models_list[[i]] <- list()
+    list_names <- c()
+    for (j in seq_along(expected_models)) {
+      data_files <- list.files(
+        path = output_folder,
+        full.names = TRUE,
+        pattern = paste0(expected_models[j], "_[0-9].rds$"))
+
+      if (length(data_files) > 0) {
+        data_lik_res <- lapply(data_files, readRDS) # nolint TODO: REMOVE
+        best_model <- choose_best_model(data_lik_res) # nolint TODO: REMOVE
+        best_models_list[[i]][[j]] <- best_model
+        list_names[j] <- expected_models[j]
+      }
+    }
+    names(best_models_list[[i]]) <- list_names
+  }
+  names(best_models_list) <- data_names
+  ranked_models <- list()
+  sorted_ranked_models <- list()
+
+  ranked_models <- lapply(best_models_list, function(x) {
+    sort(sapply(x, "[[", 9), decreasing = FALSE)
+  })
+
+  names_ranked_models <- lapply(ranked_models, names)
+
+  name_best_fit_model <- unname(sapply(names_ranked_models, "[[", 1))
+
+  sens_best_fit <- sapply(
+    name_best_fit_model,
+    identical,
+    name_best_fit_model[1])
+
+  if (all(sens_best_fit)) {
+    output$best_fit_sensitivity <- "Best fit model is not sensitive to input"
   } else {
-    output_folder <- file.path(getwd(), "results", data_name)
+    output$best_fit_sensitivity <- "Best fit model is sensitive to input"
   }
 
-  data_1_files <- list.files(
-    path = output_folder,
-    full.names = TRUE,
-    pattern = paste0(model_1, "_[0-9].rds$"))
-  model_1_lik_res <- lapply(model_1_files, readRDS) # nolint TODO: REMOVE
+  sens_model_select <- sapply(
+    names_ranked_models,
+    identical,
+    names_ranked_models[[1]])
 
-  data_2_files <- list.files(
-    path = output_folder,
-    full.names = TRUE,
-    pattern = paste0(model_2, "_[0-9].rds$"))
-  model_2_lik_res <- lapply(model_2_files, readRDS) # nolint TODO: REMOVE
+  if (all(sens_model_select)) {
+    output$model_selection_sensitivity <-
+      "Model selection rank is not sensitive to input"
+  } else {
+    output$model_selection_sensitivity <-
+      "Model selection rank is sensitive to input"
+  }
 
-  best_model_1 <- choose_best_model(model_1_lik_res) # nolint TODO: REMOVE
-  best_model_2 <- choose_best_model(model_2_lik_res) # nolint TODO: REMOVE
-  # rank models from each data set
+  output$model_selection_rank <- ranked_models
 
-  # compare ranks
-
-  #
-
-  # Take multiple data sets
-  # Take a set of models
-  # Fit the data sets to the models
-  # Determine whether the best fit model is consistent
-  # Determine whether the rank of the models fit is consistent
+  if (full_output) {
+    output$full_output <- best_models_list
+  }
+  return(output)
 }

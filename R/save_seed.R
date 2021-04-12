e94155bcba3be716848ac90a97621314ebaeaf1a
joshwlambert <j.w.l.lambert@rug.nl>
2021-04-12 16:57:28 +0200

    added save_seed

---
+++ R/save_seed.R
@@ -0,0 +1,21 @@
+#' Saves the state of the RNG
+#'
+#' @return Nothing, only saves the state of the RNG in the global environment
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' save_seed()
+#' }
+save_seed <- function() {
+  if (is_on_cluster()) {
+    file_path <- file.path(
+      Sys.getenv("HOME"), "results", "rng_state"
+    )
+  } else {
+    file_path <- file.path("results", "rng_state")
+  }
+
+  rng_state <- .GlobalEnv$.Random.seed
+  saveRDS(rng_state, file = file_path)
+}

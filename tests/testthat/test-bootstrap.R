9b8631fa2ff88f96a517fdf69df89b3880b69e2f
Neves-P <p.m.santos.neves@rug.nl>
2021-04-13 14:55:34 +0200

    Fix path

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -19,7 +19,7 @@ test_that("bootstrap works", {
   results_folder <- dirname(results_name)
   expect_true(all(file.copy(reference_files, results_folder)))
 
-  rng_state_path <- file.path(getwd(), "testdata/rng_state.rds")
+  rng_state_path <- file.path(getwd(), "testdata/rng_state/rng_state.rds")
   rng_state_temp_dir <- file.path("results", "rng_state")
   dir.create(path = rng_state_temp_dir, recursive = TRUE)
   expect_true(all(file.copy(rng_state_path, rng_state_temp_dir)))

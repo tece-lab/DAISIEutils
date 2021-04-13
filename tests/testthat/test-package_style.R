5742d7151064b74919fc1148adfca2637b93968f
joshwlambert <j.w.l.lambert@rug.nl>
2021-04-13 23:11:49 +0200

    removed goodness of fit functions and documentation and unskipped style check

--- tests/testthat/test-package_style.R
+++ tests/testthat/test-package_style.R
@@ -1,5 +1,4 @@
 test_that("Style is good", {
-  skip("temp skip")
-  #skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
+  skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
   lintr::expect_lint_free()
 })

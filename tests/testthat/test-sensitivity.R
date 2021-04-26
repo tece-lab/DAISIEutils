50cc80fc138317207935f4924101e257ff35c660
Neves-P <p.m.santos.neves@rug.nl>
2021-04-26 17:16:29 +0200

    Add test-sensitivity

--- tests/testthat/test-sensitivity.R
+++ tests/testthat/test-sensitivity.R
@@ -1,20 +1,39 @@
 test_that("sensitivity works", {
-skip("WIP")
-  data_name <- "Azores"
-  model <- "cr_dd"
-  seed <- 1
-  cond <- 1
-  results_folder <- dirname(create_output_folder(
-    data_name = data_name,
-    model = model,
-    seed = seed
-  ))
+  skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
+  data_names <- c("Azores", "Azores_alt_m")
 
-  # Place files need to run bootstrap
-  reference_files <- list.files("testdata/", full.names = TRUE)
-  expect_true(all(file.copy(reference_files, results_folder)))
+  # Place files need for sensitivity
+  reference_files <- list.files(
+    file.path(getwd(), "testdata/"), full.names = TRUE
+  )
+  azores_filepath <- create_output_folder(
+    data_name = data_names[1],
+    model = "cr_di",
+    array_index = 1
+  )
+  azores_alt_m_filepath <- create_output_folder(
+    data_name = data_names[2],
+    model = "cr_di",
+    array_index = 1
+  )
 
+  azores_folder <- dirname(azores_filepath)
+  expect_true(all(file.copy(reference_files, azores_folder)))
 
+  azores_alt_m_folder <- dirname(azores_alt_m_filepath)
+  expect_true(all(file.copy(reference_files, azores_alt_m_folder)))
+
+  sensitivity(data_names = data_names, full_output = TRUE)
+
+  obtained_result <- readRDS(file.path(
+    getwd(), "results", "Azores_Azores_alt_m", "Azores_Azores_alt_m.rds")
+  )
+
+  expected_result <- readRDS(file.path(
+    getwd(), "testdata/Azores_Azores_alt_m.rds")
+  )
+
+  expect_equal(object = obtained_result, expected = expected_result)
 
   # Delete temp folder
   expect_equal(unlink(file.path(getwd(), "results"), recursive = TRUE), 0)

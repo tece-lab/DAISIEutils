38959ab020e468937d217e1aef5ef8b6e6684909
Neves-P <p.m.santos.neves@rug.nl>
2021-04-30 01:31:11 +0200

    Print

--- tests/testthat/test-check_repeated_seeds.R
+++ tests/testthat/test-check_repeated_seeds.R
@@ -4,6 +4,7 @@ test_that("check_repeated_seeds works no duplicates", {
   reference_path <- file.path(
     getwd(), "testdata", "testlogs", "current"
   )
+  print(reference_path)
   obtained_output <- check_repeated_seeds(logs_path = reference_path)
   expected_output <- list(
     duplicated_seeds = character(),

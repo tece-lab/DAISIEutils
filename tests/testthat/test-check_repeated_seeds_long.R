fd39b2d397e8d2266d8c6e88f3dc3883abb256b5
Neves-P <p.m.santos.neves@rug.nl>
2021-04-30 01:16:59 +0200

    Add basic no seed duplicate test Also add reference files

---
+++ tests/testthat/test-check_repeated_seeds_long.R
@@ -0,0 +1,13 @@
+test_that("check_repeated_seeds_long works no duplicates", {
+  skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
+
+  reference_path <- file.path(
+    getwd(), "testdata", "testlogs", "old"
+  )
+  obtained_output <- check_repeated_seeds_long(logs_path = reference_path)
+  expected_output <- list(
+    duplicated_seeds = character(),
+    duplicated_array_indices = character()
+  )
+  expect_identical(object = obtained_output, expected = expected_output)
+})

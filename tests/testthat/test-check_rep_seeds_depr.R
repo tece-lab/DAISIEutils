b2d685222c777d5f54b2b3e2a7650ee721785f9a
Neves-P <p.m.santos.neves@rug.nl>
2021-04-30 19:04:32 +0200

    check_repeated_seeds_long -> check_rep_seeds_depr

--- tests/testthat/test-check_rep_seeds_depr.R
+++ tests/testthat/test-check_rep_seeds_depr.R
@@ -1,11 +1,11 @@
-test_that("check_repeated_seeds_long works no duplicates", {
+test_that("check_rep_seeds_depr works no duplicates", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
 
   reference_path <- file.path(
     getwd(), "testdata", "testlogs", "no_dups", "deprecated"
   )
 
-  obtained_output <- check_repeated_seeds_long(logs_path = reference_path)
+  obtained_output <- check_rep_seeds_depr(logs_path = reference_path)
   expected_output <- data.frame(
     "Data" = character(),
     "Models" = character(),
@@ -15,14 +15,14 @@ test_that("check_repeated_seeds_long works no duplicates", {
   expect_identical(object = obtained_output, expected = expected_output)
 })
 
-test_that("check_repeated_seeds_long works with duplicates", {
+test_that("check_rep_seeds_depr works with duplicates", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
 
   reference_path <- file.path(
     getwd(), "testdata", "testlogs", "dups", "deprecated"
   )
 
-  obtained_output <- check_repeated_seeds_long(logs_path = reference_path)
+  obtained_output <- check_rep_seeds_depr(logs_path = reference_path)
   expected_output <- data.frame(
     "Data" = "y_c_min",
     "Models" = "cr_dd_0laa",

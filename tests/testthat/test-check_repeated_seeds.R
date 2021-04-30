58b62959384e8e5cef80ffd3adb05d87f577f184
Neves-P <p.m.santos.neves@rug.nl>
2021-04-30 15:33:57 +0200

    Test duplicates and non duplicates

--- tests/testthat/test-check_repeated_seeds.R
+++ tests/testthat/test-check_repeated_seeds.R
@@ -2,15 +2,32 @@ test_that("check_repeated_seeds works no duplicates", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
 
   reference_path <- file.path(
-    getwd(), "testdata", "testlogs", "current"
+    getwd(), "testdata", "testlogs", "no_dups", "current"
   )
-  print("short")
-  print(reference_path)
-  print(list.files(reference_path))
+
   obtained_output <- check_repeated_seeds(logs_path = reference_path)
-  expected_output <- list(
-    duplicated_seeds = character(),
-    duplicated_array_indices = character()
+  expected_output <- data.frame(
+    "Data" = character(),
+    "Models" = character(),
+    "Seeds" = numeric(),
+    "Array_indices" = numeric()
+  )
+  expect_identical(object = obtained_output, expected = expected_output)
+})
+
+test_that("check_repeated_seeds works with duplicates", {
+  skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
+
+  reference_path <- file.path(
+    getwd(), "testdata", "testlogs", "dups", "current"
+  )
+
+  obtained_output <- check_repeated_seeds(logs_path = reference_path)
+  expected_output <- data.frame(
+    "Data" = "Aldabra_Group",
+    "Models" = "cr_di",
+    "Seeds" = 2994,
+    "Array_indices" = 9
   )
   expect_identical(object = obtained_output, expected = expected_output)
 })
--- tests/testthat/test-check_repeated_seeds_long.R
+++ tests/testthat/test-check_repeated_seeds_long.R
@@ -2,7 +2,7 @@ test_that("check_repeated_seeds_long works no duplicates", {
   skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
 
   reference_path <- file.path(
-    getwd(), "testdata", "testlogs", "old"
+    getwd(), "testdata", "testlogs", "no_dups", "old"
   )
   print("long")
   print(reference_path)
@@ -11,6 +11,25 @@ test_that("check_repeated_seeds_long works no duplicates", {
   expected_output <- list(
     duplicated_seeds = character(),
     duplicated_array_indices = character()
+  )
+  expect_identical(object = obtained_output, expected = expected_output)
+})
+
+test_that("check_repeated_seeds_long works with duplicates", {
+  skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
+
+  reference_path <- file.path(
+    getwd(), "testdata", "testlogs", "dups", "old"
+  )
+  print("long")
+  print(reference_path)
+  print(list.files(reference_path))
+  obtained_output <- check_repeated_seeds_long(logs_path = reference_path)
+  expected_output <- data.frame(
+    "Data" = "y_c_min",
+    "Models" = "cr_dd_0laa",
+    "Seeds" = 2922,
+    "Array_indices" = 4
   )
   expect_identical(object = obtained_output, expected = expected_output)
 })

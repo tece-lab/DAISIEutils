a487b272d1a37277da087273320c55591ed81475
Neves-P <p.m.santos.neves@rug.nl>
2021-05-05 21:39:38 +0200

    Add reference test

--- tests/testthat/test-read_model_results.R
+++ tests/testthat/test-read_model_results.R
@@ -1,3 +1,34 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("read_model_results works", {
+  skip_if(Sys.getenv("CI") == "", message = "Run only on CI")
+  reference_path <- file.path(getwd(), "testdata", "results")
+  obtained_results <- read_model_results(reference_path)
+
+  expected_results <- list(
+    "Azores" = list(
+      "alt_m_cr_dd" =
+        data.frame(
+          "lambda_c" = 0.048520710927281,
+          "mu" = 2.35595149303422,
+          "K" = 0.703664856086461,
+          "gamma" = 0.140370611735041,
+          "lambda_a" = 1.55697333312365,
+          "loglik" = -79.616849852223,
+          "df" = 5,
+          "conv" = 0,
+          "bic" = 196.941997409774
+        ), "alt_m_cr_di" =
+        data.frame(
+          "lambda_c" = 3.22797967279519e-08,
+          "mu" = 2.31704163523973,
+          "K" = Inf,
+          "gamma" = 0.132897281407031,
+          "lambda_a" = 1.55330115299497,
+          "loglik" = -80.058029429177,
+          "df" = 4,
+          "conv" = 0,
+          "bic" = 190.282697022616
+        )
+    )
+  )
+  expect_equal(object = obtained_results, expected = expected_results)
 })

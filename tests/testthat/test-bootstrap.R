1aca8844a6f1a699f014e6a4c651c80dc1a80424
Neves-P <p.m.santos.neves@rug.nl>
2021-11-25 17:50:02 +0100

    Temp print to confirm

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -37,7 +37,8 @@ test_that("bootstrap works", {
   expected_output <- readRDS(
     file.path("testdata/results/Azores/Azores_boot_cr_dd_1.rds")
   )
-
+  print(file.path("testdata/results/Azores/Azores_boot_cr_dd_1.rds"))
+  print(results_name)
   expect_equal(actual_output, expected_output)
 
   # Delete temp folder

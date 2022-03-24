fe38b89b4994053a1f296e8afb92b4ed6dfe08b4
Neves-P <p.m.santos.neves@rug.nl>
2022-03-24 17:02:13 +0100

    Project name and clean up improvements

--- scripts/upload_results.R
+++ scripts/upload_results.R
@@ -1,5 +1,11 @@
 # Uploads results directory to google drive
 # Make sure to manually configure token first
+
+project_name <- basename(getwd())
+message(
+  "Attempting to upload results and logs from ", project_name,
+  " to google drive.")
+
 results_present <- dir.exists("results")
 logs_present <- dir.exists("logs")
 if (isTRUE(all(logs_present, results_present))) {
@@ -14,12 +20,13 @@ if (isFALSE("googledrive" %in% rownames(installed.packages()))) {
 }
 
 time_suffix <- format(Sys.time(), "%H%M_%d%m%Y")
-results_upload_name <- paste0("results_", time_suffix)
-logs_upload_name <- paste0("logs_", time_suffix)
+results_upload_name <- paste("results", project_name, time_suffix, sep = "_")
+logs_upload_name <- paste("logs_", project_name, time_suffix, sep = "_")
 
 # Use cached token
 options(gargle_oauth_email = TRUE)
 
+# Upload files
 googledrive::drive_upload(
   media = "results.zip",
   path = paste0("inflow_r/", results_upload_name)
@@ -31,9 +38,10 @@ googledrive::drive_upload(
 )
 
 # Clean up
-file.remove("results.zip")
-file.remove("logs.zip")
+if (isTRUE(all(file.remove("results.zip"), file.remove("logs.zip")))) {
+  message("Temporary files deleted successfully.")
+}
 
-if (isFALSE(any(file.exists("results.zip"), file.exists("logs.zip")))) {
-
+if (isTRUE(any(file.exists("results.zip"), file.exists("logs.zip")))) {
+  stop("Unable do delete temporary .zip files. Please delete manually.")
 }

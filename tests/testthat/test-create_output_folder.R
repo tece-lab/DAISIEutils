09a37f0d1ab83c9dbfc1a59851a268dd0c0f4e0e
Neves-P <p.m.santos.neves@rug.nl>
2022-06-04 01:37:09 +0200

    Improve test-create_output_folder

--- tests/testthat/test-create_output_folder.R
+++ tests/testthat/test-create_output_folder.R
@@ -2,14 +2,13 @@ test_that("Directory is created", {
   data_name <- "test"
   model <- "cr_dd"
   array_index <- 1
-  file_name <- create_output_folder(
-    data_name = data_name,
-    model = model,
-    array_index = array_index
+  expect_message(file_name <- create_output_folder(
+      data_name = data_name
+    ), regexp = "results/test not found, creating it."
   )
   expect_equal(
     file_name,
-    file.path("results", "test", "test_cr_dd_1.rds")
+    file.path("results", "test")
   )
   expect_true(dir.exists(file.path(getwd(), "results", "test")))
   # Clean-up

e74908543b024c19827b698874ea31836226606c
Neves-P <p.m.santos.neves@rug.nl>
2022-06-15 16:36:26 +0200

    Update reference values

--- tests/testthat/test-read_model_results.R
+++ tests/testthat/test-read_model_results.R
@@ -3,32 +3,35 @@ test_that("read_model_results works", {
   reference_path <- file.path("testdata", "results")
   obtained_results <- read_model_results(reference_path)
 
+
   expected_results <- list(
-    "Azores" = list(
-      "alt_m_cr_dd" =
-        data.frame(
-          "lambda_c" = 0.048520710927281,
-          "mu" = 2.35595149303422,
-          "K" = 0.703664856086461,
-          "gamma" = 0.140370611735041,
-          "lambda_a" = 1.55697333312365,
-          "loglik" = -79.616849852223,
-          "df" = 5,
-          "conv" = 0,
-          "bic" = 196.941997409774
-        ), "alt_m_cr_di" =
-        data.frame(
-          "lambda_c" = 3.22797967279519e-08,
-          "mu" = 2.31704163523973,
-          "K" = Inf,
-          "gamma" = 0.132897281407031,
-          "lambda_a" = 1.55330115299497,
-          "loglik" = -80.058029429177,
-          "df" = 4,
-          "conv" = 0,
-          "bic" = 190.282697022616
-        )
-    )
+    "alt_m/Azores_alt_m_cr_dd" =
+      data.frame(
+        "lambda_c" = 0.00257412457018444,
+        "mu" = 2.35602234733843,
+        "K" = 0.873719229252812,
+        "gamma" = 0.140372944642941,
+        "lambda_a" = 1.55702668804627,
+        "loglik" = -79.6168498481904,
+        "df" = 5,
+        "conv" = 0,
+        "bic" = 196.941997401708
+      ), "alt_m/Azores_alt_m_cr_di" =
+      data.frame(
+        "lambda_c" = 3.99557658227302e-08,
+        "mu" = 2.32644864773932,
+        "K" = Inf,
+        "gamma" = 0.134810550341568,
+        "lambda_a" = 1.53244170879051,
+        "loglik" = -79.88722407779,
+        "df" = 4,
+        "conv" = 0,
+        "bic" = 189.941086319842
+      )
   )
-  expect_equal(object = obtained_results, expected = expected_results)
+
+  expect_equal(
+    object = obtained_results$Azores[5:6],
+    expected = expected_results
+  )
 })

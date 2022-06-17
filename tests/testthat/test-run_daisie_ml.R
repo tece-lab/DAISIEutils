77f500fa4a3f07071e4f9c81bad193f22e1e17d4
Neves-P <p.m.santos.neves@rug.nl>
2022-06-17 10:49:07 +0200

    Update reference

--- tests/testthat/test-run_daisie_ml.R
+++ tests/testthat/test-run_daisie_ml.R
@@ -40,15 +40,16 @@ test_that("integration test", {
     results_folder, "Azores_cr_dd_1.rds")
   )
   expected_data_frame <- data.frame(
-    lambda_c = 0.255152525210057,
-    mu = 2.37865891764784,
-    K = 0.69085076285686,
-    gamma = 0.0410410972670184,
-    lambda_a = 1.55073608640901,
-    loglik = -100.335327768561,
+    lambda_c = 0.280535793053737,
+    mu = 2.38942198362333,
+    K = 0.732758822363135,
+    gamma = 0.0413447514828757,
+    lambda_a = 1.55082618008487,
+    loglik = -100.286741139873,
     df = 5,
     conv = 0,
-    bic = 244.39881726408)
+    bic = 244.301644006703
+  )
   expect_equal(obtained_result, expected_data_frame)
   # Clean-up
   expect_equal(unlink(temp_dir, recursive = TRUE), 0)

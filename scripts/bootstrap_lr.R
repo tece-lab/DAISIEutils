f13f57a223b19dbc9acdb81ff23657fc978d30d2
Neves-P <p.m.santos.neves@rug.nl>
2022-06-20 18:55:15 +0200

    Fix argument order

--- scripts/bootstrap_lr.R
+++ scripts/bootstrap_lr.R
@@ -2,7 +2,7 @@ args <- commandArgs(TRUE)
 
 data_name <- do.call(data, args = list(args[1], package = args[4]))
 
-if (identical(args[6], "NULL")) {
+if (identical(args[7], "NULL")) {
   DAISIEutils::bootstrap_lr(
     daisie_data = get(data_name),
     data_name = data_name,
@@ -14,7 +14,7 @@ if (identical(args[6], "NULL")) {
     methode = args[8],
     optimmethod = args[9]
   )
-} else if (identical(args[6], "NA")){
+} else if (identical(args[7], "NA")){
   DAISIEutils::bootstrap_lr(
     daisie_data = get(data_name),
     data_name = data_name,
@@ -34,7 +34,7 @@ if (identical(args[6], "NULL")) {
     model_2 = args[3],
     array_index = as.numeric(args[5]),
     cond = as.numeric(args[6]),
-    results_dir = args[7],
+    results_dir = args[6],
     methode = args[8],
     optimmethod = args[9]
   )

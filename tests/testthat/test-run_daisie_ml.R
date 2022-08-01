7fb24d31c6839f0cbe4ac0e18dd5659fb75b96c3
Neves-P <p.m.santos.neves@rug.nl>
2022-08-01 16:04:16 +0200

    Add folder name to path

--- tests/testthat/test-run_daisie_ml.R
+++ tests/testthat/test-run_daisie_ml.R
@@ -28,7 +28,7 @@ test_that("integration test", {
   )))
 
   obtained_result <- readRDS(file.path(
-    results_folder, "Azores_cr_dd_1.rds"
+    results_folder, "Azores", "Azores_cr_dd_1.rds"
   ))
   expected_data_frame <- data.frame(
     lambda_c = 0.280535793053737,
